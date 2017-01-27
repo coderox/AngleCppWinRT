5786887ee11bc1005c6a39383bae3be32062076a
Johan Lindfors <johan.lindfors@coderox.se>
2017-01-27 13:03:17 +0100

    Restructured pch

--- Angle.CppWinRT/pch.h
+++ Angle.CppWinRT/pch.h
@@ -2,30 +2,7 @@
 
 #pragma comment(lib, "windowsapp") 
 
-//#include "winrt/Windows.Storage.Pickers.h"
-//#include "winrt/Windows.Storage.Streams.h"
 #include "winrt/Windows.UI.Core.h"
 #include "winrt/Windows.UI.Xaml.Controls.h"
-//#include "winrt/Windows.UI.Xaml.Media.Imaging.h"
-//#include "winrt/Windows.Graphics.Imaging.h"
 
-#include <robuffer.h>
-#include <future>
-
-#include <memory>
-#include <wrl.h>
-
-// Enable function definitions in the GL headers below
-#define GL_GLEXT_PROTOTYPES
-
-// OpenGL ES includes
-#include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
-
-// EGL includes
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-#include <EGL/eglplatform.h>
-
-// ANGLE include for Windows Store
-#include <angle_windowsstore.h>
+#include "angle.h"
