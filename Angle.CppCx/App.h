277561b85fae51907f8d757bfd2381ba60d31180
Johan Lindfors <johan.lindfors@coderox.se>
2017-01-27 08:30:33 +0100

    C++/CX project

---
+++ Angle.CppCx/App.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include <string>
+
+#include "pch.h"
+#include "SimpleRenderer.h"
+
+namespace Angle
+{
+    ref class App sealed : public Windows::ApplicationModel::Core::IFrameworkView
+    {
+    public:
+        App();
+
+        // IFrameworkView Methods.
+        virtual void Initialize(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView){}
+        virtual void SetWindow(Windows::UI::Core::CoreWindow^ window);
+		virtual void Load(Platform::String^ entryPoint) {}
+		virtual void Uninitialize() {}
+		virtual void Run() {}
+
+    private:
+		void RunApp();
+		void RecreateRenderer();
+
+        // Application lifecycle event handlers.
+        //void OnActivated(Windows::ApplicationModel::Core::CoreApplicationView^ applicationView, Windows::ApplicationModel::Activation::IActivatedEventArgs^ args);
+
+        // Window event handlers.
+        void OnWindowVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
+        void OnWindowClosed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CoreWindowEventArgs^ args);
+
+        void InitializeEGL(Windows::UI::Core::CoreWindow^ window);
+        void CleanupEGL();
+
+        bool mWindowClosed;
+        bool mWindowVisible;
+        
+        EGLDisplay mEglDisplay;
+        EGLContext mEglContext;
+        EGLSurface mEglSurface;
+
+        std::unique_ptr<SimpleRenderer> mCubeRenderer;
+    };
+
+}
