665a8c34bbd7e90826971916b1a1ed4612ca9d12
Johan Lindfors <johan.lindfors@coderox.se>
2018-05-24 11:17:36 +0200

    Xaml project added, compiles and runs, no angle yet though

---
+++ Angle.Xaml.CppWinRT/OpenGLES.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// EGL includes
+#include <EGL/egl.h>
+#include <EGL/eglplatform.h>
+
+class OpenGLES {
+public:
+	OpenGLES();
+	virtual ~OpenGLES();
+
+	void Initialize();
+	EGLSurface CreateSurface(winrt::Windows::UI::Xaml::Controls::SwapChainPanel const& panel, const winrt::Windows::Foundation::Size* renderSurfaceSize, const float* renderResolutionScale);
+	void GetSurfaceDimensions(const EGLSurface surface, EGLint* width, EGLint* height);
+	void DestroySurface(const EGLSurface surface);
+	void MakeCurrent(const EGLSurface surface);
+	EGLBoolean SwapBuffers(const EGLSurface surface);
+	void Reset();
+
+private:
+	void Cleanup();
+
+	EGLDisplay mEglDisplay;
+	EGLContext mEglContext;
+	EGLConfig mEglConfig;
+};
